434788e9ef5945e5bb1c18d98a1383e2fd061ae1
Lee HyunJoo <wayhome250@gmail.com>
2018-03-25 21:17:27 +0900

    04.c. old_app 자동생성 마이그레이션 파일 수정 - FK 수정

--- cars/migrations/0003_auto_20180325_1108.py
+++ cars/migrations/0003_auto_20180325_1108.py
@@ -9,16 +9,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cars', '0001_initial'),
+        # custom migration 을 먼저 수행한 이후에 해당 migration을 실행하도록 dependency를 변경한다
+        ('cars', '0002_auto_20180325_1112'),
+        # Make certain the tires db state is setup
+        ('tires', '0001_initial.py'),
     ]
 
+    # 해당 마이그레이션은 모델 FK 를 변경했을때 자동으로 생성된 것이다.
+    # We need to remove the DeleteModel operation because that model exists in state only.
     operations = [
         migrations.AlterField(
             model_name='car',
             name='tires',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tires.Tires'),
         ),
-        migrations.DeleteModel(
-            name='Tires',
-        ),
+
+        # tires 모델 삭제는 다음 마이그레이션에서 수행한다
     ]
