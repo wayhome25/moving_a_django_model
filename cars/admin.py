aef50253acf87663761e06225e0fbe1c4a4275ee
Lee HyunJoo <wayhome250@gmail.com>
2018-03-25 18:55:16 +0900

    02. admin, 레코드 추가

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from cars.models import Car, Tires
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'tires_name')
+
+    def tires_name(self, obj):
+        return obj.tires.name
+
+
+@admin.register(Tires)
+class TiresAdmin(admin.ModelAdmin):
+    list_display = ('name', 'size')
